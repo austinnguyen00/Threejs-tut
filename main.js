70db95ae97085f8900632ae32dd9517375b58e4c
Austin <ndkhiem3010@gmail.com>
2023-05-31 23:56:43 +0800

    Adding new objects using texture mapping + Adjust lights

--- main.js
+++ main.js
@@ -18,17 +18,19 @@ renderer.setPixelRatio(window.devicePixelRatio);
 renderer.setSize(window.innerWidth, window.innerHeight);
 
 camera.position.setZ(30);
+camera.position.setX(-3);
 
 // CREATE OBJECT
 const geometry = new THREE.TorusGeometry(10, 3, 16, 100);
-const material = new THREE.MeshStandardMaterial({ color: 0x1aa7ec });
+const material = new THREE.MeshStandardMaterial({ color: 0xff6347 });
 const torus = new THREE.Mesh(geometry, material);
 scene.add(torus);
 
 // ADDING LIGHT
-const pointLight = new THREE.PointLight(0xfffff, 1);
-const ambientLight = new THREE.AmbientLight(0xfffff);
-pointLight.position.set(10, 10, 10);
+const pointLight = new THREE.PointLight(0xffffff);
+pointLight.position.set(5, 5, 5);
+
+const ambientLight = new THREE.AmbientLight(0xffffff);
 scene.add(pointLight, ambientLight);
 
 // ADDING POINT LIGHT HELPER
@@ -61,6 +63,23 @@ Array(200).fill().forEach(addStar)
 // LOADING IMAGES TEXTURES
 const spaceTexture = new THREE.TextureLoader().load('space.jpg');
 scene.background = spaceTexture
+
+// AVATAR - TEXTURE MAPPING
+const avatarTexture = new THREE.TextureLoader().load('avatar.jpg');
+const avatar = new THREE.Mesh(
+  new THREE.BoxGeometry(3, 3, 3),
+  new THREE.MeshBasicMaterial({ map: avatarTexture })
+)
+scene.add(avatar)
+
+// MOON
+const moonTexture = new THREE.TextureLoader().load('moon.jpg');
+const normalTexture = new THREE.TextureLoader().load('normal.jpg');
+const moon = new THREE.Mesh(
+  new THREE.SphereGeometry(3, 32, 32),
+  new THREE.MeshStandardMaterial({ map: moonTexture, normalMap: normalTexture }),
+)
+scene.add(moon)
 
 // UPDATE SCREEN EVERY TIME ITS UPDATED
 function animate() {
